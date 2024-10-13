acb7b1de87828ef542e4fcb0cfe876a926597fe5
Jim Mitchell <jim@jimmitchell.org>
2024-10-13 10:47:49 -0700

    try to get masonry.js to work

--- static/js/micro-masonry.js
+++ static/js/micro-masonry.js
@@ -4,6 +4,6 @@ window.onload = function() {
         itemSelector: 'li',   // The elements Masonry will position
         columnWidth: '.masonry li', // Use the width of the items for column size
         percentPosition: true, // Use percentages for responsive layout
-        gutter: 0             // Space between items
+        //gutter: 0             // Space between items
     });
 };
