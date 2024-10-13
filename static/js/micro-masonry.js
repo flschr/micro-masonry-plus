b55538348ca0cd234c1652eca702cade6d1c3c38
Jim Mitchell <jim@jimmitchell.org>
2024-10-13 11:11:42 -0700

    final tweaks, I hope

--- static/js/micro-masonry.js
+++ static/js/micro-masonry.js
@@ -1,9 +1,8 @@
 window.onload = function() {
-    // Initialize Masonry after the window loads
     var masonry = new Masonry('.masonry', {
-        itemSelector: 'li',   // The elements Masonry will position
-        columnWidth: '.masonry li', // Use the width of the items for column size
-        percentPosition: true, // Use percentages for responsive layout
-        //gutter: 0             // Space between items
+        itemSelector: 'li',
+        columnWidth: '.masonry li',
+        percentPosition: true,
+        gutter: 0
     });
 };
