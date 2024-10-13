fbb5a3e87b7db27d080330097b3c41b52416624c
Jim Mitchell <jim@jimmitchell.org>
2024-10-13 09:21:03 -0700

    try to get masonry.js to work

--- static/js/micro-masonry.js
+++ static/js/micro-masonry.js
@@ -2,7 +2,7 @@ var elem = document.querySelector('.grid');
 var msnry = new Masonry( elem, {
   // options
   itemSelector: '.grid-item',
-  columnWidth: 300
+  columnWidth: 200
 });
 
 // element argument can be a selector string
