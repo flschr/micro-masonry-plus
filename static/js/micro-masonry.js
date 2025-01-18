23c92bd7f34d0a3cd850c678b6cb1b50383387ca
Jim Mitchell <jim@jimmitchell.org>
2025-01-18 13:34:52 -0800

    complete change of approach

--- static/js/micro-masonry.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var masonry = new Masonry('.grid', {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid .grid-item',
-        percentPosition: true,
-        gutter: 0
-    });
-};
